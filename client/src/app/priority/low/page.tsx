4119e99ee82317f52ce4b287eabea32abbb1d9aa
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2024-10-07 15:02:40 -0400

    init

---
+++ client/src/app/priority/low/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReusablePriorityPage from '../resuablePriorityPage';
+import { Priority } from '@/state/api';
+
+const Urgent = () => {
+  
+    return <ReusablePriorityPage priority={Priority.Low} />; // Fixed the JSX closing tag
+
+};
+
+export default Urgent;
