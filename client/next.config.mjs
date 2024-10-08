e24a80946de1a0d03ce051e1324d134561337fb4
Richard Haar <35735904+itsrichardhaar@users.noreply.github.com>
2024-10-08 11:07:16 -0400

    next config mjs to work with s3 images

---
+++ client/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: "project-management-s3-images.s3.amazonaws.com",
+                port: "",
+                pathname: "/**",
+            }
+        ]
+    }
+};
+
+export default nextConfig;
